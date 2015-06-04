5901955c4ea28fa254888028ffaf0bf8e1a7bcf2
Adam Turner <apps@adamturner.org>
2015-06-03 17:58:22 -0700

    Add Google Analytics script to the analytics function

--- nc-functions.php
+++ nc-functions.php
@@ -58,9 +58,18 @@ function nc_edit_author_caps() {
  * @uses wp_head()
  * @since 0.2
  */
-function nc_add_google_analytics() {
-    echo '<!-- Google Analytics code here -->';
-}
-add_action('wp_head', 'nc_add_google_analytics');
+function nc_add_google_analytics() { ?>
+<!-- NC Functions Plugin - Google Analytics -->
+<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-63746201-1', 'auto');
+	ga('send', 'pageview');
+</script>
+<?php }
+add_action('wp_head', 'nc_add_google_analytics', 15);
 
 /* DON'T DELETE THIS CLOSING TAG */ ?>
